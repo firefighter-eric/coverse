969ffe8ff9745aecd70a13db5822656cd9c20054
Qifan Zhang <firefighter-eric@users.noreply.github.com>
2025-12-20 23:05:13 +0800

    style

--- coverse/agents/multi_agent.py
+++ coverse/agents/multi_agent.py
@@ -9,39 +9,39 @@ from coverse.agents.converse_agent import ConverseAgent
 class MultiAgentChat:
     def __init__(self, agents: list[ConverseAgent]):
         self.agents = agents
-        logger.info(f'Loaded {len(agents)} agents')
+        logger.info(f"Loaded {len(agents)} agents")
         for i, agent in enumerate(agents):
-            logger.info(f'agent {i+1}: {agent.agent_name}')
+            logger.info(f"agent {i + 1}: {agent.agent_name}")
 
     def chat_with_agent(self, multi_agent_messages, agent):
         # change role to agent_name
         messages = deepcopy(multi_agent_messages)
         for msg in messages:
-            if msg['role'] == agent.agent_name:
-                msg['role'] = 'assistant'
+            if msg["role"] == agent.agent_name:
+                msg["role"] = "assistant"
             else:
-                msg['role'] = 'user'
+                msg["role"] = "user"
         # chat
         answer = agent.run(messages)
         return answer
 
-    def run(self, first_message: str = '', n_turns: int = 5, verbose: bool = False):
-        messages = [{'role': 'user', 'content': first_message}]
+    def run(self, first_message: str = "", n_turns: int = 5, verbose: bool = False):
+        messages = [{"role": "user", "content": first_message}]
         for i in tqdm(range(n_turns), disable=not verbose):
             for agent in self.agents:
                 answer = self.chat_with_agent(multi_agent_messages=messages, agent=agent)
-                messages.append({'role': agent.agent_name, 'content': answer})
+                messages.append({"role": agent.agent_name, "content": answer})
         return messages
 
 
-if __name__ == '__main__':
-    MODEL_NAME = 'qwen3:4b-instruct'
-    agent_1 = ConverseAgent(agent_name='agent_1', model_name=MODEL_NAME)
-    agent_2 = ConverseAgent(agent_name='agent_2', model_name=MODEL_NAME)
-    agent_3 = ConverseAgent(agent_name='agent_3', model_name=MODEL_NAME)
+if __name__ == "__main__":
+    MODEL_NAME = "qwen3:4b-instruct"
+    agent_1 = ConverseAgent(agent_name="agent_1", model_name=MODEL_NAME)
+    agent_2 = ConverseAgent(agent_name="agent_2", model_name=MODEL_NAME)
+    agent_3 = ConverseAgent(agent_name="agent_3", model_name=MODEL_NAME)
     client = MultiAgentChat([agent_1, agent_2, agent_3])
     first_message = "小猫在阳光下睡觉"
 
     messages = client.run(first_message=first_message, n_turns=5, verbose=True)
     for m in messages:
-        print(f'{m["role"]}: {m["content"]}')
+        print(f"{m['role']}: {m['content']}")
--- coverse/prob_detect/prob_detect.py
+++ coverse/prob_detect/prob_detect.py
@@ -1,4 +1,3 @@
-
 from transformers import AutoTokenizer, AutoModelForMaskedLM, FillMaskPipeline
 from pprint import pprint
 from loguru import logger
@@ -11,7 +10,8 @@ tokenizer = AutoTokenizer.from_pretrained(model_path)
 model = AutoModelForMaskedLM.from_pretrained(model_path, device_map="auto")
 fill_mask_pipeline = FillMaskPipeline(model=model, tokenizer=tokenizer)
 
-# %% 
+# %%
+
 
 def detect_one_token_prob(text: str, target: str, index: int = 0):
     results = fill_mask_pipeline(text, targets=[target], top_k=1)
@@ -24,25 +24,28 @@ def detect_prob(text, target):
     target_tokens = [tokenizer.decode([token_id]) for token_id in target_token_ids]
     logger.info(f"Target tokens: {target_tokens}")
 
-    output = {'text': text, 'target': target, 'token_results': []}
+    output = {"text": text, "target": target, "token_results": []}
     for i in range(len(target_tokens)):
         target_token = target_tokens[i]
         result = detect_one_token_prob(text, target=target_token, index=i)
-        output['token_results'].append(result)
+        output["token_results"].append(result)
 
     prob = 1.0
     log_prob = 0.0
-    for token_res in output['token_results']:
-        prob *= token_res['score']
-        log_prob += -1.0 * np.log(token_res['score'] + 1e-12)
-    output['prob'] = prob
-    output['log_prob'] = log_prob
-                
+    for token_res in output["token_results"]:
+        prob *= token_res["score"]
+        log_prob += -1.0 * np.log(token_res["score"] + 1e-12)
+    output["prob"] = prob
+    output["log_prob"] = log_prob
+
     return output
 
-texts = ["学习，就像[MASK][MASK][MASK]，因为久了方觉甜",
-          "学习，就像[MASK][MASK][MASK]",
-         "学习，就像[MASK][MASK][MASK]，因为都很困难",]
+
+texts = [
+    "学习，就像[MASK][MASK][MASK]，因为久了方觉甜",
+    "学习，就像[MASK][MASK][MASK]",
+    "学习，就像[MASK][MASK][MASK]，因为都很困难",
+]
 
 target = "嚼馒头"
 
@@ -56,7 +59,9 @@ for text in texts:
 
 # summary
 for output in outputs:
-    print(f"Prob: {output['prob']:.2e} | Log Prob: {output['log_prob']:.2f} | Text: {output['text']} | Target: {output['target']}")
+    print(
+        f"Prob: {output['prob']:.2e} | Log Prob: {output['log_prob']:.2f} | Text: {output['text']} | Target: {output['target']}"
+    )
 
 
 with open("data/prob_detect/test_1/2.json", "w", encoding="utf-8") as f:
